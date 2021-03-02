da250084640c718528f50a508361130b058a2005
Brody Bayley <brodybayley@gmail.com>
2021-03-02 21:55:28 +0000

    add cohort with longest ARs and avg cohort assistance times queries

---
+++ 4_queries/5_avg_ar_duration_cohort.sql
@@ -0,0 +1,7 @@
+-- Average duration of AR's per cohort
+SELECT cohorts.name as name, AVG(completed_at - started_at) as average_assistance_time
+FROM students
+JOIN cohorts ON cohort_id = cohorts.id 
+JOIN assistance_requests ON student_id = students.id
+GROUP BY cohorts.name
+ORDER BY average_assistance_time;
