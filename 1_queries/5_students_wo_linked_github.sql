cb8e5044681aec9fd1aa77ae9ed892994c31d587
Brody Bayley <brodybayley@gmail.com>
2021-03-02 00:15:47 +0000

    add join queries

--- 1_queries/5_students_wo_linked_github.sql
+++ 1_queries/5_students_wo_linked_github.sql
@@ -1,6 +1,5 @@
--- SELECT name, email, phone
--- FROM students
--- WHERE end_date IS NOT NULL AND
--- github IS NULL
+SELECT name, email, phone
+FROM students
+WHERE end_date IS NOT NULL AND
+github IS NULL
 
-SELECT count(*) FROM assignment_submissions;
